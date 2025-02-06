9b992c017460ca051e19802ecc3e00f104c07734
thecahar <zaharpioner@gmail.com>
2025-02-06 18:38:33 +0000

    a

--- app/database.py
+++ app/database.py
@@ -4,7 +4,6 @@ import os
 
 DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+asyncpg://user:password@localhost/dbname")
 
-# Create async engine and session
 engine = create_async_engine(DATABASE_URL, echo=True)
 async_session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
